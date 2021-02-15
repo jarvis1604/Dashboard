98e2c23ef05e1965c824eb93fb6d98fc2bf7a709
Apurva Churi <apurvachuri95@gmail.com>
2021-02-15 23:31:33 +1030

    adding json files

---
+++ src/assets/dev_specific.js
@@ -0,0 +1,520 @@
+export const setupData={
+	"code": 200,
+	"status": 200,
+	"time": "2021-01-11T09:47:53+0000Z",
+	"correlationId": "bfa3db60-1b64-4132-a9e0-57dc467ec222",
+	"data": {
+		"envs": [
+			{
+				"type": "Dev",
+				"subtype": "odo",
+				"alias": "odo11d",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "15",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "10/09/2020 15:30:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},
+			{
+				"type": "Dev",
+				"subtype": "odo",
+				"alias": "odo11d",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "15",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},
+            {
+				"type": "Dev",
+				"subtype": "odo",
+				"alias": "odo11d",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "14",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 17:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},{
+				"type": "Dev",
+				"subtype": "odo",
+				"alias": "odo11d",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "13",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},{
+				"type": "Dev",
+				"subtype": "odo",
+				"alias": "odo11d",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "14",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},{
+				"type": "Dev",
+				"subtype": "odo",
+				"alias": "odo11d",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "14",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},
+			{
+				"type": "IFT",
+				"subtype": "odo",
+				"alias": "odo1t",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "12",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},
+			{
+				"type": "SQI",
+				"subtype": "odo",
+				"alias": "odo1t",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "14",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},
+			{
+				"type": "SIT",
+				"subtype": "odo",
+				"alias": "odo1t",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "12",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},
+			{
+				"type": "SVT",
+				"subtype": "odo",
+				"alias": "odo1t",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "11",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},
+			{
+				"type": "Staging",
+				"subtype": "odo",
+				"alias": "odo1t",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "10",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 18:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			},
+			{
+				"type": "PROD",
+				"subtype": "odo",
+				"alias": "odo1t",
+				"deployments": [
+					{
+						"deploymentId": "abc3db60-1b64-4132-a9e0-57dc467ec111",
+						"satus": "inprogress",
+						"sprintnumber": "",
+						"buildnumber": "11",
+						"startTime": "04/09/2020 14:00:00",
+						"endTime": "14/09/2020 16:20:30",
+						"installmode": "refresh_cd",
+						"milestones": [
+							{
+								"name": "preinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-asm-setup.yml",
+								"status": "not started",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "dop-p2cc-prepare.yml",
+								"status": "inprogress",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							},
+							{
+								"name": "postinstall",
+								"status": "completed",
+								"starttime": "2021-01-11T09:40:53+0000Z",
+								"endtime": "2021-01-11T09:47:53+0000Z"
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	"request": {
+		"params": {
+			"TDB": "TBD"
+		},
+		"data": {},
+		"method": "GET",
+		"path": "/deployments",
+		"requestId": "f40869f8-2b26-4499-9ef0-125f2c8b814c"
+	}
+};
